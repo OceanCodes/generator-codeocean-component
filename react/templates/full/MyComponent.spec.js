26ac80d12b195ddc29387230778398fffd87ac49
jony89 <ariel@emath.co.il>
2017-10-26 14:08:03 +0300

    enhancing tests

--- react/templates/full/MyComponent.spec.js
+++ react/templates/full/MyComponent.spec.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import <%= componentName %> from './<%= componentName %>';
 
-it('should have div element', () => {
-  const wrapper = shallow(<<%= componentName %> />);
-  expect(wrapper.find('div')).toBeDefined();
+describe('<%= componentName %>', () => {
+  it('should have div element', () => {
+    const wrapper = shallow(<<%= componentName %> />);
+    expect(wrapper.find('div')).toBeDefined();
+  });
 });
